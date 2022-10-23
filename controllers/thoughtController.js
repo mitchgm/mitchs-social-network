53a31853273b38112964d73311781157fdd3715e
mitchgm <mitchgm@bgsu.edu>
2022-10-23 14:08:19 -0700

    thoughtCont setup

---
+++ controllers/thoughtController.js
@@ -0,0 +1,14 @@
+const { Thought, User } = require("../models");
+
+const thoughtCont = {
+    // keeps our controller functions within our export
+
+
+
+
+
+};
+
+module.exports = thoughtCont
+
+// this exports our thoughtCont 
